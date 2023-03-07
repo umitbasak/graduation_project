9ee33322cf8435c9690a0625be9022cf6a3c048e
umitbasak <umit.basak123@gmail.com>
2023-03-08 00:58:01 +0300

    Code is updated to handle traverse files under a directory recursively

---
+++ xlsxContractName.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const path = require("path");
+const parser = require("@solidity-parser/parser");
+const xlsx = require("xlsx");
+
+// Directory containing Solidity files
+const directory = "./morpho/morpho-v1-main/src";
+
+let xlsxData = [
+  [
+    "File Name",
+    "Contract Name",
+    "Internal Imports",
+    "External Imports",
+    "Functions",
+    "State Variables",
+  ],
+];
+
+function countStats(filePath) {
+  if (fs.statSync(filePath).isDirectory()) {
+    fs.readdirSync(filePath).forEach((file) =>
+      countStats(path.join(filePath, file))
+    );
+  } else if (path.extname(filePath) === ".sol") {
+    const source = fs.readFileSync(filePath, "utf8");
+    const ast = parser.parse(source, { loc: true });
+    let contractName;
+    let numInternalImports = 0;
+    let numExternalImports = 0;
+    let numFunctions = 0;
+    let numStateVariables = 0;
+    ast.children.forEach((node) => {
+      if (node.type === "ContractDefinition") {
+        contractName = node.name;
+
+        node.subNodes.forEach((subNode) => {
+          //   console.log(subNode.type);
+          if (subNode.type === "FunctionDefinition") {
+            numFunctions++;
+          } else if (subNode.type === "StateVariableDeclaration") {
+            numStateVariables += subNode.variables.length;
+          }
+        });
+      } else if (node.type === "ImportDirective") {
+        const importedFile = node.path;
+        if (importedFile.startsWith("./") || importedFile.startsWith("../")) {
+          numInternalImports++;
+        } else {
+          numExternalImports++;
+        }
+      }
+    });
+    xlsxData.push([
+      filePath,
+      contractName,
+      numInternalImports,
+      numExternalImports,
+      numFunctions,
+      numStateVariables,
+    ]);
+  }
+}
+countStats(directory);
+
+console.log(xlsxData);
+
+let workbook = xlsx.utils.book_new();
+let sheet = xlsx.utils.aoa_to_sheet(xlsxData);
+xlsx.utils.book_append_sheet(workbook, sheet, "Contracts");
+xlsx.writeFile(workbook, "contract_stats.xlsx");
